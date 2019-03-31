a51a4de42890b572bb2390bc381810c72555105b
Pavel Perestoronin <pavel.perestoronin@kpn.com>
2019-03-31 17:09:18 +0200

    Fix PathLike argument to sqlite3.connect in Python 3.6

--- sqlitemap/__init__.py
+++ sqlitemap/__init__.py
@@ -182,8 +182,9 @@ class Connection(ConnectionWrapper, MutableMapping[str, Collection]):
         super().__init__(dumps_, loads_)
         if isinstance(database, PathLike):
             # `sqlite3.connect` in Python 3.6 doesn't accept `PathLike`.
-            database = database.__fspath__()
-        self.connection = sqlite3.connect(database, **kwargs)
+            self.connection = sqlite3.connect(database.__fspath__(), **kwargs)
+        else:
+            self.connection = sqlite3.connect(database, **kwargs)
 
     def __len__(self) -> int:
         with closing(self.connection.execute('''
